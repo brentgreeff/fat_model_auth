c855ac2747876d8a3b4ecea702508b8bd727f396
Brent Greeff <brentgreeff@gmail.com>
2014-10-23 15:57:28 +0100

    Fixing render 404, need to specify layout as false.

--- lib/fat_model_auth/controller_helpers.rb
+++ lib/fat_model_auth/controller_helpers.rb
@@ -1,19 +1,19 @@
 module FatModelAuth
   class AuthException < Exception; end
-  
+
   module ControllerHelpers
     protected
-    
+
     def auth_required
       authority = get_authority
-      
+
       access_granted = authority.allows(current_user).send "to_#{params[:action]}?"
       respond_with_404_page unless access_granted
     end
-    
+
     def access_denied?
       authority = get_authority
-      
+
       access_granted = authority.allows(current_user).send "to_#{params[:action]}?"
       if access_granted
         false
@@ -22,9 +22,9 @@ module FatModelAuth
         true
       end
     end
-    
+
     private
-    
+
     def get_authority
       if self.respond_to? :override_authority
         authority = override_authority
@@ -34,12 +34,12 @@ module FatModelAuth
         authority = instance_variable_get("@#{authority_name}")
         raise FatModelAuth::AuthException, "#{authority_name} is nil" if authority.nil?
       end
-      
+
       return authority
     end
-    
+
     def respond_with_404_page
-      render "#{Rails.root}/public/404.html", :status => 404
+      render "#{Rails.root}/public/404.html", status: 404, layout: false
     end
   end
 end
