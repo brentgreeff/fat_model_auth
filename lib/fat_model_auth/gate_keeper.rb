6b5965a6d5321986427ba9aca86c090d68d46002
Viktar Basharymau <viktar.basharymau@thehamon.com>
2014-10-31 18:18:11 +0300

    Simplify GateKeeper#add_rules

--- lib/fat_model_auth/gate_keeper.rb
+++ lib/fat_model_auth/gate_keeper.rb
@@ -7,10 +7,8 @@ module FatModelAuth
     end
 
     def add_rules(params)
-      h = params.pop
-      auth_condition = h[:if]
-      auth_condition ||= proc {|model, user| !h[:unless].call(model, user)}
-      methods = params
+      *methods, options = params
+      auth_condition = options[:if] || negate(options[:unless])
 
       for method in methods
         @map["to_#{method}?".to_sym] = auth_condition
@@ -30,6 +28,14 @@ module FatModelAuth
       return false if @user.nil?
 
       @map[method].call(@model, @user)
+    end
+
+    private
+
+    def negate(predicate)
+      proc do |*args|
+        !predicate.call(*args)
+      end
     end
   end
 end
