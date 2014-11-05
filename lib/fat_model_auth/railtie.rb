1a65aa9dbdc62e4d75d458551da5e29c502f2da6
bublik42 <stefkin.v@gmail.com>
2014-11-05 12:01:52 +0300

    Add model and view helpers to railtie

--- lib/fat_model_auth/railtie.rb
+++ lib/fat_model_auth/railtie.rb
@@ -1,12 +1,18 @@
 require 'rails/railtie'
 
 module FatModelAuth
-  $my_rails_root = Rails.root
-  
-	class Railtie < Rails::Railtie
-		initializer "fat_model_auth.controller_helpers" do |app|
+  class Railtie < Rails::Railtie
+    initializer "fat_model_auth.controller_helpers" do |app|
       ActiveSupport.on_load :action_controller do
         include FatModelAuth::ControllerHelpers
+      end
+
+      ActiveSupport.on_load :active_record do
+        extend FatModelAuth::ModelHelpers
+      end
+
+      ActiveSupport.on_load :action_view do
+        extend FatModelAuth::ViewHelpers
       end
     end
   end
