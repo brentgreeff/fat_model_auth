a576a69d0d66cf957441680f4b76796eaf85fde3
Dzmitry Kovalenok <dzmitry.kovalenok@thehamon.com>
2015-10-08 17:11:06 +0300

    Rename get_actions -> actions

--- lib/fat_model_auth/view_helpers.rb
+++ lib/fat_model_auth/view_helpers.rb
@@ -3,14 +3,14 @@ module FatModelAuth
     def allowed_to?(options)
       authority = options.values.first
 
-      get_actions(options).any? do |action|
+      actions(options).any? do |action|
         authority.allows(current_user).send "to_#{action}?"
       end
     end
 
     private
 
-    def get_actions(options)
+    def actions(options)
       return options.keys.first.to_s.split('_or_')
     end
   end
