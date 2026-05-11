08f33cc79ac908d6e73d1dd9295486f5b4dec880
Brent <brentgreeff@gmail.com>
2026-05-11 19:30:08 +0200

    Add nil guard to `allowed_to?` with spec
    - Raise `AuthException` when authority object is nil, surfacing a clear error instead of a `NoMethodError` deep in the auth chain

--- lib/fat_model_auth/view_helpers.rb
+++ lib/fat_model_auth/view_helpers.rb
@@ -4,6 +4,7 @@ module FatModelAuth
   module ViewHelpers
     def allowed_to?(options)
       actions, authority = options.first
+      raise FatModelAuth::AuthException, "#{authority.inspect} is nil" if authority.nil?
 
       actions.to_s.split('_or_').any? do |action|
         authority.allows(current_user).send("to_#{action}?")
---
+++ spec/view_helpers_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'fat_model_auth/view_helpers'
+
+class TestViewContext
+  include FatModelAuth::ViewHelpers
+
+  attr_writer :current_user
+
+  def current_user
+    @current_user
+  end
+end
+
+RSpec.describe FatModelAuth::ViewHelpers do
+  let(:view) { TestViewContext.new }
+
+  it 'raises AuthException when authority is nil' do
+    expect { view.allowed_to?(edit: nil) }.to raise_error(FatModelAuth::AuthException)
+  end
+end
